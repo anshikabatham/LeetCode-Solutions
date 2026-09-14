b9d5d9f696dddd013f3da39e2e78f8987724f24f
ANSHIKA BATHAM <bathamanshika23@gmail.com>
2026-09-14 15:25:20 +0530

    Time: 2 ms (91.7%), Space: 47.7 MB (76.03%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        //int right=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+               nums[left]=nums[i];
+               left++;
+            }
+            
+        }
+        while(left<nums.length){
+            nums[left]=0;
+            left++;
+        }
+        
+    }
+}
