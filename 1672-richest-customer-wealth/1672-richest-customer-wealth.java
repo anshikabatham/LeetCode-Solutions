0adbe7002637b129a131975964ab9751fad343d2
ANSHIKA BATHAM <bathamanshika23@gmail.com>
2026-09-15 00:49:29 +0530

    Time: 0 ms (100%), Space: 44.4 MB (87.43%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxwealth=0;
+        for(int i=0;i<accounts.length;i++){
+            int currentcustomerwealth=0;
+            for(int j=0;j<accounts[i].length;j++){
+                currentcustomerwealth=currentcustomerwealth+accounts[i][j];
+            }
+            maxwealth=Math.max(maxwealth,currentcustomerwealth);
+        }
+        return maxwealth;
+        
+    }
+}
