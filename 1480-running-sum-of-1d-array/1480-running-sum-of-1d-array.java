2c8073cfef5b348ee1172bbbcb96fe3092cfae0a
ANSHIKA BATHAM <bathamanshika23@gmail.com>
2026-09-14 09:34:16 +0530

    Time: 0 ms (100%), Space: 44.6 MB (5.01%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] ans= new int[nums.length];
-        ans[0]=nums[0];
+        int[] pref=new int [nums.length];
+        pref[0]=nums[0];
         for(int i=1;i<nums.length;i++){
-            ans[i]=nums[i]+ans[i-1];
+            pref[i]=nums[i]+pref[i-1];
         }
-        return ans;
+        return pref;
     }
+    
 }
