31d3ce5419a5144a3ccfdbdfb1e383896bf9e5a4
ANSHIKA BATHAM <bathamanshika23@gmail.com>
2026-09-24 13:42:55 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (40.74%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+       for(int i=0;i<nums.length;i++){
+        int num=nums[i];
+        int sum=0;
+     
+        while(num>0){
+            int endno=num%10;
+            sum+=endno;
+            num/=10;
+        }
+        if(i==sum){
+         return i;
+            }
+        }
+        return -1;
+    
+         
+
+   }
+}
