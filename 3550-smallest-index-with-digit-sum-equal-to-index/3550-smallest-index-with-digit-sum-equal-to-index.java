638b3e6901092e4f66ae5605fa8eb4ff0f828331
ANSHIKA BATHAM <bathamanshika23@gmail.com>
2026-09-24 13:44:18 +0530

    Time: 1 ms (99.69%), Space: 45.5 MB (59.67%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,3 +1,6 @@
+
+         
+         
 class Solution {
     public int smallestIndex(int[] nums) {
        for(int i=0;i<nums.length;i++){
@@ -10,12 +13,10 @@ class Solution {
             num/=10;
         }
         if(i==sum){
-         return i;
+            return i;
             }
         }
         return -1;
     
-         
-
    }
 }
